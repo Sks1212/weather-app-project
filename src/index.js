ac237f3f3fc03e699cff9dd12dce44e4d2b4b5cf
BRL <sukiisuu@gmail.com>
2021-03-27 16:49:27 +0900

    notes

--- src/index.js
+++ src/index.js
@@ -56,6 +56,21 @@ function searchLocation(position) {
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
+//////////
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+//////////////
 
 
 let dateElement = document.querySelector("#date");
